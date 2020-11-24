538e41b77dbd7b167792b565186f8625bfce59a6
Scott Knick <scott.knick.ctr@progeny.net>
2020-11-24 16:40:30 -0700

    .

---
+++ Client/Web_UI/Source/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Raceday msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Raceday from './components/Raceday.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Raceday
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
