812d424d85b276e054f70f7317175395f2724e5d
Scott Knick <scott.knick.ctr@progeny.net>
2021-01-09 13:37:39 -0700

    .

--- Schema.sql
+++ Schema.sql
@@ -73,6 +73,23 @@ CREATE TABLE access_token (
     FOREIGN KEY (user_id) REFERENCES system_user(id)
 );
 
+CREATE VIEW events AS
+SELECT event.id AS event_id,
+       event.name AS event_name,
+       event.start AS event_start,
+       event.description AS event_description,
+       location.id AS location_id,
+       location.name AS location_name,
+       location.description AS location_description,
+       series.id AS series_id,
+       series.name AS series_name,
+       series.description AS series_description
+  FROM event
+  LEFT JOIN location
+    ON event.location_id = location.id
+  LEFT JOIN series
+    ON event.series_id = series.id;
+
 CREATE VIEW broadcasts AS
 SELECT broadcast.id AS broadcast_id,
        broadcast.type AS broadcast_type,
@@ -90,20 +107,3 @@ SELECT broadcast.id AS broadcast_id,
   FROM broadcast
   JOIN events
     ON broadcast.event_id = events.event_id;
-
-CREATE VIEW events AS
-SELECT event.id AS event_id,
-       event.name AS event_name,
-       event.start AS event_start,
-       event.description AS event_description,
-       location.id AS location_id,
-       location.name AS location_name,
-       location.description AS location_description,
-       series.id AS series_id,
-       series.name AS series_name,
-       series.description AS series_description
-  FROM event
-  LEFT JOIN location
-    ON event.location_id = location.id
-  LEFT JOIN series
-    ON event.series_id = series.id;
