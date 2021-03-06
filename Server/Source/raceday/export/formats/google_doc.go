344d532beabad75712f9dbd09592565528c44fa8
Scott Knick <sknick@gmail.com>
2021-03-06 07:15:25 -0700

    .

--- Server/Source/raceday/export/formats/google_doc.go
+++ Server/Source/raceday/export/formats/google_doc.go
@@ -3,8 +3,10 @@ package formats
 import (
 	"context"
 	"errors"
+	"log"
 
 	docs "google.golang.org/api/docs/v1"
+	drive "google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
 
@@ -23,23 +25,61 @@ func (gd GoogleDoc) Export() error {
 
 	ctx := context.Background()
 
-	googleService, err := docs.NewService(ctx, option.WithCredentialsFile(gd.GoogleServiceAccountKeyFile))
+	// Get service instances so we can communicate with the Google Drives and Docs APIs:
+	drivesApi, err := drive.NewService(ctx, option.WithCredentialsFile(gd.GoogleServiceAccountKeyFile))
 	if err != nil {
 		return err
 	}
 
-	docsService := docs.NewDocumentsService(googleService)
+	docsApi, err := docs.NewService(ctx, option.WithCredentialsFile(gd.GoogleServiceAccountKeyFile))
+	if err != nil {
+		return err
+	}
 
+	// For updating our new document's permissions and getting a link to it
+	permissionsService := drive.NewPermissionsService(drivesApi)
+	// filesService := drive.NewFilesService(drivesApi)
+
+	// For creating the new document
+	docsService := docs.NewDocumentsService(docsApi)
+
+	// First create the document:
 	newDoc := docs.Document{
 		Title: "Race Day Export",
 	}
 
-	req := docsService.Create(&newDoc)
+	docsCreateReq := docsService.Create(&newDoc)
 
-	_, err = req.Do()
+	doc, err := docsCreateReq.Do()
 	if err != nil {
 		return err
 	}
+
+	// Now update the document's permissions so it's viewable by anyone:
+	permCreateReq := permissionsService.Create(
+		doc.DocumentId,
+		&drive.Permission{
+			Role: "writer",
+			Type: "anyone",
+		},
+	)
+
+	_, err = permCreateReq.Do()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Created document with ID: %s", doc.DocumentId)
+
+	// // Finally, figure out the link to the new document:
+	// filesGetReq := filesService.Get(doc.DocumentId)
+
+	// f, err := filesGetReq.Do()
+	// if err != nil {
+	// 	return err
+	// }
+
+	log.Printf("Document available at: https://docs.google.com/document/d/%s", doc.DocumentId)
 
 	return nil
 }
