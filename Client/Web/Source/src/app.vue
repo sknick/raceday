f892a87601ab64904ac5795bb381dfc7928bf48b
Bryan Andersen <bryan.j.andersen@gmail.com>
2021-04-17 20:22:06 -0400

    Initial darker background and buttons, aligns day hover background color with selected

--- Client/Web/Source/src/app.vue
+++ Client/Web/Source/src/app.vue
@@ -24,10 +24,22 @@ export default {
 
 <style>
 
+:root {
+    --dp-border-radius: 6px;
+    --dp-child-border-md: 4px;
+    --dp-child-border-sm: 4px;
+    --button-background: var(--dark);
+    --button-background-hover: var(--primary);
+    --button-text-color: var(--light);
+    --button-text-color-hover: var(--secondary);
+    --button-shadow: 0 3px 6px rgba(39, 43, 48, 0.8), 0 3px 6px rgba(0, 0, 0, 0.9);
+}
+
 .app-wrapper {
     height: 100vh;
     width: 100vw;
     overflow: hidden;
 }
+@import "css/datepicker.css";
 
 </style>
