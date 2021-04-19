0c95fe95cea8769b509fbeb9e83e0baa4ff07e5e
Bryan Andersen <bryan.j.andersen@gmail.com>
2021-04-19 07:11:05 -0400

    Adds inset shadow to month and year selectors - adds box-shadow to datepicker button - adds box-shodow to mo/year nav buttons - moves dp vars to datepicker.css

--- Client/Web/Source/src/app.vue
+++ Client/Web/Source/src/app.vue
@@ -26,13 +26,14 @@ export default {
 
 :root {
     --dp-border-radius: 6px;
-    --dp-child-border-md: 4px;
+    --dp-border-radius-md: 4px;
     --dp-child-border-sm: 4px;
+    --dp-select-border-radius: 4px;
     --button-background: var(--dark);
     --button-background-hover: var(--primary);
     --button-text-color: var(--light);
     --button-text-color-hover: var(--secondary);
-    --button-shadow: 0 3px 6px rgba(39, 43, 48, 0.8), 0 3px 6px rgba(0, 0, 0, 0.9);
+    --button-shadow: 0 3px 6px rgba(0, 0, 0, 0.9);
 }
 
 .app-wrapper {
