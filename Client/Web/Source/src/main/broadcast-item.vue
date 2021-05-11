fa74942c6e19b4ddba66c74bbe14207fc40e090b
Scott Knick <sknick@gmail.com>
2021-05-10 20:34:55 -0600

    .

--- Client/Web/Source/src/main/broadcast-item.vue
+++ Client/Web/Source/src/main/broadcast-item.vue
@@ -2,13 +2,15 @@
     <div class="grid-container">
         <img :src="mediaIcon()" :title="mediaTypeText()" />
 
-        <span v-for="symbol in symbols" v-bind:key="symbol">
-            <span :title="symbol.title" v-html="symbol.htmlCode"/>
+        <span v-for="symbol in symbols" v-bind:key="symbol" class="symbol">
+            <img v-if="symbol.icon != ''" :src="symbol.icon" :title="symbol.text"/>
+            <span v-else/>
         </span>
 
         <span>
-            <span v-if="hasValidUrl()"><a :href="broadcast.url" target="_blank">{{ linkText() }}</a></span>
-            <span v-else>{{ linkText() }}</span>
+            <span v-if="hasValidUrl()" class="broadcast-text"><a :href="broadcast.url" target="_blank">{{ linkText() }}</a></span>
+            <span v-else class="broadcast-text">{{ linkText() }}</span>
+
             <span v-for="lang in langs()" v-bind:key="lang" class="lang-list">
                 <span :title="lang.id" v-html="lang.html_code"/>
             </span>
@@ -62,17 +64,17 @@ export default {
         mediaIcon() {
             switch (this.broadcast.type_) {
                 case "Cable":
-                    return require('../assets/cable.png')
+                    return require("../assets/media/cable.png")
                 case "Facebook":
-                    return require("../assets/facebook.png")
+                    return require("../assets/media/facebook.png")
                 case "F1_TV":
-                    return require("../assets/f1tv.png")
+                    return require("../assets/media/f1tv.png")
                 case "Motorsport_tv":
-                    return require("../assets/motorsporttv.png")
+                    return require("../assets/media/motorsporttv.png")
                 case "YouTube":
-                    return require("../assets/youtube.png")
+                    return require("../assets/media/youtube.png")
                 default:
-                    return require("../assets/other.png")
+                    return require("../assets/media/other.png")
             }
         },
 
@@ -91,36 +93,36 @@ export default {
     mounted() {
         if (this.broadcast.paid === true) {
             this.symbols.push({
-                htmlCode: "&#128178;",
-                title:    "Requires Account"
-            });
+                icon: require("../assets/icons/dollar.svg"),
+                text: "Requires Account"
+            })
         } else if (this.broadcast.paid === false) {
             this.symbols.push({
-                htmlCode: "&#128275;",
-                title:    "Free"
-            });
+                icon: require("../assets/icons/unlock-fill.svg"),
+                text: "Free"
+            })
         } else {
             this.symbols.push({
-                htmlCode: "",
-                title:    ""
-            });
+                icon: "",
+                text: ""
+            })
         }
 
         if (this.broadcast.geoblocked === true) {
             this.symbols.push({
-                htmlCode: "&#128683;",
-                title:    "Geoblocked"
-            });
+                icon: require("../assets/icons/geo-fill.svg"),
+                text: "Geoblocked"
+            })
         } else if (this.broadcast.geoblocked === false) {
             this.symbols.push({
-                htmlCode: "&#128275;",
-                title:    "Unrestricted"
-            });
+                icon: require("../assets/icons/globe2.svg"),
+                text: "Not Geoblocked"
+            })
         } else {
             this.symbols.push({
-                htmlCode: "",
-                title:    ""
-            });
+                icon: "",
+                text: ""
+            })
         }
     }
 }
@@ -134,14 +136,22 @@ a {
     color: #91bfe6;
 }
 
+.broadcast-text {
+    margin-left: 16px;
+}
+
 .grid-container {
     display: grid;
-    grid-template-columns: 40px 30px 30px auto;
+    grid-template-columns: 46px 26px 26px auto;
     align-items: center;
 }
 
 .lang-list {
     margin-left: 10px;
+}
+
+.symbol {
+    align: "center"
 }
 
 </style>
